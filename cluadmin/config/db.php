5f9ac4727c34cb096f7159d62ed003a53cff023d
filutfi <ltvie7s@gmail.com>
2017-06-07 14:40:26 +0700

    versi 1
    baru di upload semua

---
+++ cluadmin/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=db_clutera',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ common/config/params.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    'backendUrl'=>'utkmengambilPictureygdiuploadNantinya',
+];
+
+
+// ngambilny ntar \Yii::$app->params['backendUrl']
